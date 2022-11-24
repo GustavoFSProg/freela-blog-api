ed877495e8ceebf4a37d408bb1047919801f00b6
Gustavo <gustavo.prog40@gmail.com>
2022-11-24 08:40:23 -0300

    wwwineasfasdasf

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ const { PORT } = process.env
 
 const api = express()
 
-// api.use(cors({
-//   origin:  ['https://freela-blog.netlify.app/', 'https://freela-api-blog.herokuapp.com/']
-// }))
+api.use(cors({
+  origin:  ['https://freela-blog.netlify.app/', 'https://freela-api-blog.herokuapp.com/']
+}))
 
 // api.use(cors())
 api.use(express.json())
