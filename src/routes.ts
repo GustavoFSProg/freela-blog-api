1384e52d797ef5d2552bc0a9debdf3252a9795d4
Gustavo <gustavo.prog40@gmail.com>
2022-11-24 08:18:43 -0300

    ffffff

--- src/routes.ts
+++ src/routes.ts
@@ -12,14 +12,14 @@ routes.get('/', function (req: Request, res: Response) {
   return res.status(200).send({ message: ` 😎 Api running: ` })
 })
 
-routes.get('/all', (req: Request, res: Response) => {
-  res.setHeader('Access-Control-Allow-Origin',['https://freela-blog.netlify.app/', 'https://freela-api-blog.herokuapp.com/'])
-  // res.setHeader('Access-Control-Allow-Origin', '*')
+// routes.get('/all', (req: Request, res: Response) => {
+//   res.setHeader('Access-Control-Allow-Origin',['https://freela-blog.netlify.app/', 'https://freela-api-blog.herokuapp.com/'])
+//   // res.setHeader('Access-Control-Allow-Origin', '*')
     
 
-}, postController.getAll),
+// }, postController.getAll),
   
-  // routes.get('/all', postController.getAll),
+  routes.get('/all', postController.getAll),
   // routes.get('/total', postController.getAll),
   routes.post('/register', upload.single('image'), postController.registerPost),
   // routes.put('/likes/:id', postController.updateLikes)
